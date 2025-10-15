e8f92d4184f175db9a38afc3e4f1e97f64e810b7
sanjay E <sanjaye0408@gmail.com>
2025-10-15 18:13:10 +0530

    React and Tailwind installation

---
+++ LoginPage/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Login Page</h1>
+        <p>Backendinput tester</p>
+      </div>
+      <div>
+        <input placeholder="name"></input>
+        <input placeholder="age"></input>
+      </div>
+    </>
+  )
+}
+
+export default App

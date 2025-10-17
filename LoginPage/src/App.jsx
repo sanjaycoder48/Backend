0d6deddd424a3bd5e25513a47e700ffb62ca27ae
sanjay E <sanjaye0408@gmail.com>
2025-10-17 10:18:55 +0530

    structure

--- LoginPage/src/App.jsx
+++ LoginPage/src/App.jsx
@@ -1,18 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <div>
-           
-        </div>
-      </div>
+    <div className="flex justify-center items-center h-screen">
+      <p className="text-4xl font-bold text-gray-800">LOGIN</p>
+    </div>
     </>
   )
 }

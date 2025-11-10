31f71328ca47531bae85e8431d9ab88a50acd27e
sanjay E <sanjaye0408@gmail.com>
2025-11-10 20:38:36 +0530

    installation

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+
+app.get("/",(req,res) => {
+    res.send("Hello,world!")
+});
+
+app.listen(5000, () => {
+    console.log("server is running on port 5000");
+});
+
+ 
